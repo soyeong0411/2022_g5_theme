94f7646960e59649409874ce30e3d1bb0e17c58f
soyeong0411 <2so092@naver.com>
2022-08-25 16:26:00 +0900

    0825

--- skin/latest/basic/latest.skin.php
+++ skin/latest/basic/latest.skin.php
@@ -6,6 +6,30 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
 $list_count = (is_array($list) && $list) ? count($list) : 0;
 ?>
 
+    <div class="mylatest">
+        <div class="title">
+            <h3><a href="<?php echo get_pretty_url($bo_table); ?>"><?php echo $bo_subject ?></a></h3>
+            <a href="<?php echo get_pretty_url($bo_table); ?>" class="mIcon">icon</a>
+        </div>
+        <ul class="content">
+            <?php for ($i=0; $i<$list_count; $i++) {  ?>
+            <li>
+                <a href="<? echo get_pretty_url($bo_table, $list[$i]['wr_id']) ?>">
+                    <h4><? echo $list[$i]['subject']; ?>
+                    <? if ($list[$i]['icon_new']) echo "<span class=\"new_icon\">N<span class=\"sound_only\">새글</span></span>";?>
+                </h4>
+                    <p><?php echo $list[$i]['datetime2'] ?></p>
+                </a>
+            </li>
+            <? } ?>
+
+            <?php if ($list_count == 0) { //게시물이 없을 때  ?>
+            <li class="empty_li">게시물이 없습니다.</li>
+            <?php }  ?>
+        </ul>
+    </div>
+
+
 <div class="lat">
     <h2 class="lat_title"><a href="<?php echo get_pretty_url($bo_table); ?>"><?php echo $bo_subject ?></a></h2>
     <ul>
@@ -41,9 +65,7 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
             </div>
         </li>
     <?php }  ?>
-    <?php if ($list_count == 0) { //게시물이 없을 때  ?>
-    <li class="empty_li">게시물이 없습니다.</li>
-    <?php }  ?>
+    
     </ul>
     <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more"><span class="sound_only"><?php echo $bo_subject ?></span>더보기</a>
 
